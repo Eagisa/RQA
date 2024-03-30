df1f1fdada2ef714fd7248c217cc1f8ca7ee107c
Eagisa <117991739+Eagisa@users.noreply.github.com>
2024-03-31 02:42:52 +0530

    Add files via upload

--- RQA_FS/RQA_CBS.py
+++ RQA_FS/RQA_CBS.py
@@ -11,18 +11,19 @@ init(autoreset=True)
 import msvcrt
 import time
 
+def PTC():
+    msvcrt.getch()
+
 try:
     import RQAM
 except:
     print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> RQAM import failed!\n")
+    PTC()
+    exit()
 
 #=============================================================================#
-RCC_S = "https://raw.githubusercontent.com/Eagisa/RQA/main/RQA_S/RQA-S.json"
+RQA_S = "https://raw.githubusercontent.com/Eagisa/RQA/main/RQA_S/RQA-S.json"
 #=============================================================================#
-
-
-def PTC():
-    msvcrt.getch()
 
 def RQA_Updater():
     # Function to download a file from a URL
@@ -32,12 +33,12 @@ def RQA_Updater():
             file.write(response.content)
 
     # Fetching JSON data from the URL
-    url = RCC_S
+    url = RQA_S
     response = requests.get(url)
     data = response.json()
 
     # Getting the version of RCC-C from the JSON object
-    rqa_c_version = data.get("RCC-C", {}).get("Version")
+    rqa_c_version = data.get("RQA-C", {}).get("Version")
 
     RQA_ver = RQAM.RQA_version
 
@@ -45,8 +46,8 @@ def RQA_Updater():
     # Checking if the version matches
     if not rqa_c_version == RQA_ver and not debug:
         os.system("cls")
-        print("\n", Fore.BLACK+Back.LIGHTGREEN_EX+" R.C.C ", Fore.LIGHTYELLOW_EX+"> Updating RCC...\n")
-        updater_info = data.get("RCC-FS", {}).get("RCCMP")
+        print("\n", Fore.BLACK+Back.LIGHTGREEN_EX+" R.Q.A ", Fore.LIGHTYELLOW_EX+"> Updating RQA...\n")
+        updater_info = data.get("RQA-FS", {}).get("RQAM")
         if updater_info:
             file_url = updater_info
             filename = os.path.basename(file_url)
