ab16b21ba6ad6b60e8cb96ebdbc5c1cd4bd538a4
Eagisa <117991739+Eagisa@users.noreply.github.com>
2024-03-31 17:08:56 +0530

    Add files via upload

--- RQA_FS/RQA_INTR.py
+++ RQA_FS/RQA_INTR.py
@@ -12,40 +12,33 @@ init(autoreset=True)
 import msvcrt
 
 def PTC():
-        msvcrt.getch()
+    msvcrt.getch()
 
-#Connection to the Hosting RCC Server
-#==============================================================================#
-RQA_S = "https://raw.githubusercontent.com/Eagisa/RQA/main/RQA_S/RQA-S.json"
-#==============================================================================#
+def download_file(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
 
+def check_files_exist():
+    # Files to check for existence
+    files_to_check = ['RQAM.py', 'RQA_CBS.py']
 
-def RQA_INTR_Laucher():
-    def check_files_exist():
-        # Files to check for existence
-        files_to_check = ['RQAM.py', 'RQA_CBS.py']
-        
-        # Check if all files exist
-        missing_files = []
-        for filename in files_to_check:
-            if not os.path.exists(filename):
-                missing_files.append(filename)
-        return missing_files
+    # Check if all files exist
+    missing_files = []
+    for filename in files_to_check:
+        if not os.path.exists(filename):
+            missing_files.append(filename)
+    return missing_files
 
-    def download_file(file_url, file_path):
-        response = requests.get(file_url, stream=True)
-        with open(file_path, 'wb') as f:
-            for data in response.iter_content(chunk_size=4096):
-                f.write(data)
-
-    # Example usage:
+def RQA_INTR_Launcher():
     missing_files = check_files_exist()
     if missing_files:
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> Installing RQA-Files...\n")
         for filename in missing_files:
             file_url = f"https://raw.githubusercontent.com/Eagisa/RQA/main/RQA_FS/{filename}"
             download_file(file_url, filename)
-        
+
         os.system("cls")
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> Successfully installed RCC-Files\n")
         os.system("cls")
@@ -57,7 +50,7 @@ def RQA_INTR_Laucher():
 def Lauch_RQA():
     try:
         import RQAM
-    except:
+    except ImportError:
         os.system("cls")
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> (RQA_INTR) Failed to import RQAM\n")
         PTC()
@@ -67,7 +60,7 @@ def Lauch_RQA():
 def Lauch_RQA_Updater():
     try:
         import RQA_CBS
-    except:
+    except ImportError:
         os.system("cls")
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> (RQA_INTR) Failed to import RQA_CBS\n")
         PTC()
@@ -80,35 +73,28 @@ def RQA_CBS_Updater():
     response = requests.get(url)
     data = response.json()
     server_ver = data.get("RQA-CBS", {}).get("Version")
-    
+
     try:
         import RQA_CBS
-    except:
+    except ImportError:
         os.system("cls")
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> (RQA_INTR) Failed to import RQA_CBS\n")
         PTC()
         exit()
 
-    # Get local version directly from the imported module
     local_ver = RQA_CBS.RQA_CBS_Version
 
-    # Check if the file exists
     if local_ver is not None:
         try:
-            # Check if the local version matches the server version
             if local_ver != server_ver:
                 os.system("cls")
                 print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> Updating RQA_CBS...\n")
 
-                # URL to download RCC_CBS file from the JSON data
                 rcc_fs = data.get("RQA-FS", {})
                 rcc_cbs_url = rcc_fs.get("RQA_CBS")
 
                 if rcc_cbs_url:
-                    # Save the downloaded file in the current directory
                     file_save_path = os.path.join(os.getcwd(), 'RQA_CBS.py')
-
-                    # Download the file
                     download_file(rcc_cbs_url, file_save_path)
                     os.system("cls")
 
@@ -129,8 +115,5 @@ def RQA_CBS_Updater():
     else:
         print("\n", Fore.BLACK + Back.LIGHTGREEN_EX + " R.Q.A ", Fore.LIGHTYELLOW_EX + "> Error missing 'RQA_CBS' file\n")
 
-def download_file(url, save_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as f:
-            f.write(response.content)
+# Connection to the Hosting RCC Server
+RQA_S = "https://raw.githubusercontent.com/Eagisa/RQA/main/RQA_S/RQA-S.json"
